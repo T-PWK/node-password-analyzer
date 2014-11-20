c72e0fb9dd47d228fa29561782084404c34f2557
T-PWK <tompwk@gmail.com>
2014-11-20 22:04:39 +0000

    Functionality improvements

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 var util = require('util');
 var analyzers = require('./lib/analyzers');
+var Group = require('./lib/group');
+
 var test = ['123456', 'foobar', 'xyz', 'FOOBAR', 'FooBar34'];
 
 function analyze (passwords) {
 
-	var g = new Group();
-	console.log(g)
-
-	g.addAnalyzer(new analyzers.DigitsOnlyAnalyzer());
-	g.addAnalyzer(new analyzers.LettersOnlyAnalyzer());
-	g.addAnalyzer(new analyzers.CapitalLettersOnlyAnalyzer());
-	g.addAnalyzer(new analyzers.LowerLettersOnlyAnalyzer());
+	var analyzer = new PasswordAnalyzer();
+	analyzer.addGroup('foo bar 1', analyzers.DigitsOnlyAnalyzer);
+	analyzer.addGroup('foo bar 2', 'numeric');
 
 	passwords.forEach(function (passwd) {
-		g.analyze(passwd);
+		analyzer.analyze(passwd);
 	});
 
-	console.log(g);
-	console.log('=========================')
-
+	console.log(util.inspect(analyzer, {depth: 5, colors: true}));
 }
 
-function Group (name, analyzers) {
-	this.name = name || '[group]';
-	this._results = { total: 0 };
-	this._analyzers = [];
+function PasswordAnalyzer (options) {
+	options = options || {};
+	this._groups = [];
+}
 
-	if(util.isArray(analyzers)) {
-		this._analyzers = this._analyzers.concat(analyzers);
-	} else if(analyzers != null) {
-		this.analyzers.push(analyzers);
+PasswordAnalyzer.prototype.addGroup = function (name, analyzersOrFn) {
+	if (!analyzersOrFn) { 
+		throw new Error('Analyzers are required');
+	}
+	var group;
+
+	for (var i = this._groups.length - 1; i >= 0; i--) {
+		if(this._groups[i].name === name) {
+			group = this._groups[i];
+			break;
+		}
+	};
+
+	if(!group) {
+		group = new Group(name);
+		this._groups.push(group);
 	}
 
-	this._analyzers.forEach(function (analyzer) {
-		this._results[analyzer.code] = 0;
-	}, this);
-}
-
-Group.prototype.addAnalyzer = function (analyzer) {
-	this._analyzers.push(analyzer);
-	this._results[analyzer.code] = 0;
+	if(util.isArray(analyzersOrFn)) {
+		analyzersOrFn.forEach(group.addAnalyzer.bind(group));
+	}
+	else {
+		group.addAnalyzer(analyzersOrFn);
+	}
 };
 
-Group.prototype.analyze = function (password) {
-	this._results.total++;
-	this._analyzers.forEach(function (analyzer) {
-		if(analyzer.analyze(password)) {
-			this._results[analyzer.code]++;
-		}
-	}, this);
+PasswordAnalyzer.prototype.analyze = function (password) {
+	this._groups.forEach(function (group) {
+		group.analyze(password);
+	});
 };
 
 analyze(test);
