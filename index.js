3e5241aab2e364147c35497251869f79178fa9bf
T-PWK <tompwk@gmail.com>
2014-11-22 00:47:39 +0000

    Group class moved to the index.js

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 var util = require('util');
 var analyzers = require('./lib/analyzers');
-var Group = require('./lib/group');
 
-var test = ['123456', 'foobar', 'xyz', 'FOOBAR', 'FooBar34', 'January', 'mArcH'];
+// function analyze (passwords) {
 
-function analyze (passwords) {
-
-	var analyzer = new PasswordAnalyzer();
-	analyzer.addGroup('Character sets', ['numeric', 'loweralpha', 'upperalpha']);
-	analyzer.addGroup('Hashcat masks', ['numeric', 'loweralpha', 'upperalpha']);
-	analyzer.addGroup('Months', ['months']);
+// 	var analyzer = new PasswordAnalyzer();
+// 	analyzer.addGroup('Character sets', ['numeric', 'loweralpha', 'upperalpha']);
+// 	analyzer.addGroup('Hashcat masks', ['numeric', 'loweralpha', 'upperalpha']);
+// 	analyzer.addGroup('Months', ['months']);
 	
 
-	passwords.forEach(function (passwd) {
-		analyzer.analyze(passwd);
-	});
+// 	passwords.forEach(function (passwd) {
+// 		analyzer.analyze(passwd);
+// 	});
 
-	console.log(util.inspect(analyzer, {depth: 5, colors: true}));
-}
+// 	console.log(util.inspect(analyzer, {depth: 5, colors: true}));
+// }
 
 function PasswordAnalyzer (options) {
 	options = options || {};
@@ -56,4 +53,38 @@ PasswordAnalyzer.prototype.analyze = function (password) {
 	});
 };
 
-analyze(test);
+function Group (name, analyzers) {
+	this.name = name || '[group]';
+	this._results = { total: 0 };
+	this._analyzers = [];
+
+	if(util.isArray(analyzers)) {
+		this._analyzers = this._analyzers.concat(analyzers);
+	} else if(analyzers) {
+		this.analyzers.push(analyzers);
+	}
+}
+
+Group.prototype.addAnalyzer = function (analyzer) {
+	if(util.isFunction(analyzer)) {
+		this._analyzers.push(new analyzer);
+	}
+	else if(typeof analyzer === 'string') {
+		this._analyzers.push(new analyzers[analyzer]);
+	}
+	else {
+		this._analyzers.push(analyzer);
+	}
+};
+
+Group.prototype.analyze = function (password) {
+	this._results.total++;
+	this._analyzers.forEach(function (analyzer) {
+		analyzer.analyze(password, this._results);
+	}, this);
+};
+
+module.exports = {
+	PasswordAnalyzer: PasswordAnalyzer,
+	analyzers: analyzer
+}
